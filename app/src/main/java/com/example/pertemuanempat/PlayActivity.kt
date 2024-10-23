0a4735a35ae22b491950ace6c35a8f37b2d029ad
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2024-10-23 14:20:32 +0700

    menambahkan logo dan icon check

--- app/src/main/java/com/example/pertemuanempat/PlayActivity.kt
+++ app/src/main/java/com/example/pertemuanempat/PlayActivity.kt
@@ -1,10 +1,24 @@
 package com.example.pertemuanempat
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview(showBackground = true)
 @Composable
@@ -18,5 +32,18 @@ fun PlayActivity(
 
 @Composable
 fun SectionHeader(){
-
+    Box(modifier = Modifier.fillMaxWidth()
+        .background(color = Color.LightGray)){
+        Row {
+            Box(contentAlignment = Alignment.BottomEnd){
+                Image(painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "",
+                    Modifier.size(100.dp)
+                )
+                Icon(Icons.Filled.Check,
+                    contentDescription = "",
+                    Modifier.padding(15.dp))
+            }
+        }
+    }
 }
